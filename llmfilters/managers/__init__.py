6a80297009835c09fcecd10450cd41d96440acb1
piratos <sayf.piratos@gmail.com>
2023-07-09 20:23:12 +0200

    remove debug

--- llmfilters/managers/__init__.py
+++ llmfilters/managers/__init__.py
@@ -1,5 +1,5 @@
 import importlib
-from llmfilters.managers.simple import SimpleManager
+from llmfilters.managers.simple_manager import SimpleManager
 
 __all__ = [
     'SimpleManager',
@@ -7,17 +7,16 @@ __all__ = [
 
 try:
     import pika
-    from llmfilters.managers.rabbitmq import RabbitMQManager
+    from llmfilters.managers.rabbitmq_manager import RabbitMQManager
     __all__.append('RabbitMQManager')
 except ImportError:
     pass
 
 try:
     import kafka
-    from llmfilters.managers.kafka import KafkaManager
+    from llmfilters.managers.kafka_manager import KafkaManager
     __all__.append('KafkaManager')
 except ImportError:
-    raise
     pass
 
 
